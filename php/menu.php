dbdd35e0c0fc440bb979171a4666ae5a51459d71
Roderic Linguri <linguri@digices.com>
2016-08-21 16:34:51 -0600

    Update menu.php

--- php/menu.php
+++ php/menu.php
@@ -51,7 +51,7 @@ foreach ($menu as $dept=>$dcat) {
     
     $htm .= '          <div id="dept-sect-'.$d.'-0-0" class="dept-sect">'.PHP_EOL;
     $htm .= '            <div id="dept-head-'.$d.'-0-0" class="dept-head">'.PHP_EOL;
-    $htm .= '              <a href="index.php?dept='.$d.'">'.str_replace('_',' ',ucwords($dept)).'</a>';
+    $htm .= '              <a href="index.php?dept='.$d.'">'.ucwords(str_replace('_',' ',$dept)).'</a>';
     
     if (isset($dcat)) {
         $htm .= '<span id="dept-disc-'.$d.'-0-0" class="dept-disc"></span>';
@@ -67,7 +67,7 @@ foreach ($menu as $dept=>$dcat) {
 
 			$htm .= '              <div id="catg-sect-'.$d.'-'.$c.'-0" class="catg-sect">'.PHP_EOL;
 			$htm .= '                <div id="catg-head-'.$d.'-'.$c.'-0" class="catg-head">'.PHP_EOL;
-			$htm .= '                  <a href="index.php?dept='.$d.'&catg='.$c.'">'.str_replace('_',' ',ucwords($catg)).'</a><span id="catg-disc-'.$d.'-'.$c.'-0" class="catg-disc"></span>'.PHP_EOL;
+			$htm .= '                  <a href="index.php?dept='.$d.'&catg='.$c.'">'.ucwords(str_replace('_',' ',$catg)).'</a><span id="catg-disc-'.$d.'-'.$c.'-0" class="catg-disc"></span>'.PHP_EOL;
 			$htm .= '                </div><!--./catg-head-->'.PHP_EOL;
 			$htm .= '                <div id="catg-body-'.$d.'-'.$c.'-0" class="catg-body">'.PHP_EOL;
 
@@ -75,7 +75,7 @@ foreach ($menu as $dept=>$dcat) {
 			foreach ($citm as $item) {
 			
 				$htm .= '                  <div id="item-sect-'.$d.'-'.$c.'-'.$i.'" class="item-sect" onclick="window.location = \'index.php?dept='.$d.'&catg='.$c.'&item='.$i.'\'">'.PHP_EOL;
-				$htm .= '                    <a href="index.php?dept='.$d.'&catg='.$c.'&item='.$i.'">'.str_replace('_',' ',ucwords($item)).'</a>'.PHP_EOL;
+				$htm .= '                    <a href="index.php?dept='.$d.'&catg='.$c.'&item='.$i.'">'.ucwords(str_replace('_',' ',$item)).'</a>'.PHP_EOL;
 				$htm .= '                  </div><!--./item-sect-->'.PHP_EOL;
 				$i++;
 
